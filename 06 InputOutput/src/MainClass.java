27ca7b766c52e57dacd1566bf66405354d18e578
mehdi <mehdi_adeli@aut.ac.ir>
2023-06-20 11:07:26 +0000

    added chapter 6 Input and Output

--- 06 InputOutput/src/MainClass.java
+++ 06 InputOutput/src/MainClass.java
@@ -1,7 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Enter text");
         Scanner scanner=new Scanner(System.in);
         String name=scanner.nextLine();
@@ -24,6 +29,38 @@ public class MainClass {
         boolean b=scanner.hasNext();
         System.out.println(b);
 
-        
+
+        number=5;
+        String str="mehdi";
+        System.out.println("my name is "+str+" and i am "+number+" years old");
+        System.out.printf("my name is %s and I am %d years old",str,number);
+        System.out.printf("\nmy name is %s and I am %d years %h old",str,number,15);
+        System.out.printf("\nmy name is %s and I am %d years %x old",str,number,15);
+        System.out.printf("\n%d %x %o %%",15,15,15);
+        System.out.printf("\n%+d %x %o %%",15,15,15);
+        System.out.printf("\n%(+,d %#x %#o %%",15254,15,15);
+        number=15254;
+        System.out.printf("\n%(+,d %1$d %1$x %1$o %%",number);
+        System.out.printf("\n%d %<x %1$d %1$x %1$o %%",number);
+
+        String str2=String.format("my name is %s and I am %d years old",str,number);
+        System.out.println(str2);
+
+        PrintWriter out=new PrintWriter("a.txt","UTF-8");
+        out.println("Hello Itpro");
+        out.println("Hello Itpro");
+        out.println("Hello Itpro");
+        out.println("Hello Itpro");
+        out.println("Hello Itpro");
+        out.println("Hello Itpro");
+        out.close();
+        Scanner scanner1=new Scanner(Paths.get("a.txt"),"UTF-8");
+        System.out.println(scanner1.nextLine());
+        System.out.println(scanner1.nextLine());
+        System.out.println(scanner1.nextLine());
+        System.out.println(scanner1.nextLine());
+        System.out.println(scanner1.nextLine());
+        System.out.println("finished");
+
     }
 }
